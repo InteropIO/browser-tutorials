dc852e76de5b8739977e921385b362ec97002ebc
Kalina Georgieva <kalina.georgieva@interop.io>
2026-03-12 14:58:37 +0200

    Revert file
    Signed-off-by: Kalina Georgieva <kalina.georgieva@interop.io>

--- javascript/solution/client-details/index.js
+++ javascript/solution/client-details/index.js
@@ -42,9 +42,9 @@ const start = async () => {
             if (context.client) {
                 setFields(context.client);
                 myWorkspace.setTitle(context.client.name);
-            }
+            };
         });
-    }
+    };
 };
 
 start().catch(console.error);
